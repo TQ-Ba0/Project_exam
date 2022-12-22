5b88753cfca82b364868b3d2c115c3d493a4e171
Orson <dangcaoho151202@gmail.com>
2022-12-22 17:21:15 +0700

    update welcome

--- Welcome.py
+++ Welcome.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd
 
-
 st.set_page_config(
     page_title='Welcome',
     page_icon="👋"
@@ -10,16 +9,62 @@ st.write("# Data analysis of national high school exam scores ! 👋")
 st.sidebar.success("Select a demo above")
 st.markdown(
        """
-    This is a dataset about test scores that candidates achieved in the college entrance examination for the school 
-     year from 2019 to 2021 in Vietnam. You can view statistics by subject and exam block
-    ### Data Soure?
-    - The data is taken from the github link of the author **BeeCost Trợ lý Mua Sắm** [Link GitHub](https://github.com/beecost/bee-university/tree/master/output_data/crawler/common) 👈
+    ---
+    ### Source
+    - The data is taken from the github link of the author **BeeCost Trợ lý Mua Sắm** [GitHub Link](https://github.com/beecost/bee-university/tree/master/output_data/crawler/common) 👈
+    ---
+    ### About Dataset
+    ### Subject
+    This is a dataset about test scores that candidates achieved in the college entrance examination for the school year 2019 to 2021 in Vietnam.
+    You can view statistics by subject and exam block.
+
+    ### Content
+    The dataset includes the student's identification number and the corresponding score of that student:
+
+    sbd - Candidate number
+
+    Van - Literature score
+
+    Toan - Mathematics score
+
+    Ma_mon_ngoai_ngu - Foreign language code
+
+    Ngoai_ngu - Foreign language score
+
+    - N1: English
+
+    - N2: Russian
+
+    - N3: French
+
+    - N4: Chinese
+
+    - N5: German
+
+    - N6: Japanese
+
+    Li - Physics score
+
+    Hoa - Chemistry score
+
+    Sinh - Biology score
+
+    Su - History score
+
+    Dia - Geography score
+
+    GDCD - Civics score
+
+    ### Author's words
+    Hope this dataset helps the data analysis community.
+
+    ---
     ### Members
-    - Teams have 4 people:
-        - Trần Quốc Bảo
-        - Hồ Đăng Cao 
-        - Đỗ Đức Duy
-        - Ngô Hữu Nhật Thanh
+    - The Website's designed by 4 people:
+        - Trần Quốc Bảo - 20127449
+        - Hồ Đăng Cao - 20127452
+        - Đỗ Đức Duy - 20127479
+        - Ngô Hữu Nhật Thanh - 20127
 
 """
 )
--- pages/1_Subject.py
+++ pages/1_Subject.py
@@ -7,11 +7,11 @@ st.set_page_config(
     page_title="Subject"
 )
 
-img =Image.open('images/Subject.png')
+img = Image.open('images/Subject.png')
 st.image(img,use_column_width=True)
-st.sidebar.header('User Input  Features')
+st.sidebar.header('User Input Features')
 st.title("Subject")
-selected_year=st.sidebar.selectbox('Year',list(reversed(range(2019,2021))))
+selected_year = st.sidebar.selectbox('Year',list(reversed(range(2019,2021))))
 st.markdown( """
     On this page you can see the statistics of each subject and each year by choice
     """
@@ -19,15 +19,17 @@ st.markdown( """
 @st.cache
 def load_data(year):
     path_file='diemthi'+str(year)+'.csv'
-    temp=pd.read_csv(path_file)
-    df=pd.DataFrame(temp)
+    temp = pd.read_csv(path_file)
+    df = pd.DataFrame(temp)
     df.drop(df.columns[[0]], axis=1, inplace=True)
     return df
-select=load_data(selected_year)
-sort_columns=np.array(select.columns)
-select_subject=st.sidebar.multiselect('Subject',sort_columns ,sort_columns)
-st.write('Data Dimension:' +str(select.shape[0]) + ' row and ' + str(select.shape[1]) + ' columns')
-df_exam=select[select_subject]
+
+select = load_data(selected_year)
+sort_columns = np.array(select.columns)
+select_subject = st.sidebar.multiselect('Subject',sort_columns,sort_columns)
+
+st.write('Data Dimension: ' + str(select.shape[0]) + ' rows and ' + str(select.shape[1]) + ' columns')
+df_exam = select[select_subject]
 st.markdown("## Display Data type of DataFrame")
 
 st.dataframe(df_exam)
